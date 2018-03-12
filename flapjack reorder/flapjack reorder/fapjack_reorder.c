254c68a8acebb7e09d2409710f0056c6d8b87343
DamonSandy <33623888+DamonSandy@users.noreply.github.com>
2018-03-12 22:58:18 +0800

    add program project

---
+++ flapjack reorder/flapjack reorder/fapjack_reorder.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include"flapjack_reorder.h"
+unsigned char my_flapjack[FLAPJACK_NUM] = {5,3,13,9,12,19,14,134,53,32,212,176};
+void main()
+{
+	unsigned char i = 0;
+	Reorder_flapjack(my_flapjack,FLAPJACK_NUM);
+	for(i=0; i<FLAPJACK_NUM; i++){
+		printf("%d\n",my_flapjack[i]);
+	}
+
+}
+void Insert_Num_To_Orderly(
+	unsigned char *flapjack_size,
+	unsigned char flapjack_num,  //当前数组包含数据的数量（小于等于数组大小）
+	unsigned char insert_num     //需要插入的数据
+)
+{
+	unsigned char i = 0;
+	unsigned char j = 0;
+	while(i < flapjack_num){
+		if(flapjack_size[i] > insert_num){
+			j = i;
+			break;
+		}
+		i++;
+	}
+	if(i < flapjack_num){//插入的数据小于当前最大的数
+		for(i=flapjack_num; i>j; i--){
+			flapjack_size[i] = flapjack_size[i-1];
+		}
+		flapjack_size[j] = insert_num;
+	}
+	else{//插入的数据大于当前最大的数
+		flapjack_size[i] = insert_num;
+	}
+}
+void Reorder_flapjack(
+	unsigned char *my_flapjack_size,
+	unsigned char my_flapjack_num  //烙饼个数
+)
+{
+	unsigned char i = 1;
+	for(i=1; i<my_flapjack_num; i++){
+		Insert_Num_To_Orderly(my_flapjack_size,i,my_flapjack_size[i]);
+	}
+}
---
+++ flapjack reorder/flapjack reorder/flapjack_reorder.h
@@ -0,0 +1,7 @@
+#ifndef  _FLAPJACK_REORDER_
+#define _FLAPJACK_REORDER_
+
+#define FLAPJACK_NUM  12   //烙饼数量
+void Insert_Num_To_Orderly(unsigned char *flapjack_size,unsigned char flapjack_num,unsigned char insert_num); //将一个数插入一个有序的数组中
+void Reorder_flapjack(unsigned char *my_flapjack_size,unsigned char my_flapjack_num);
+#endif
